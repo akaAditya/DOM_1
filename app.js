f468f3e815b91a5b0c6fd06410b0d2caab4a9b59
akaAditya <mymail.adityapatel@gmail.com>
2023-05-20 01:39:45 +0530

    fourth commit adding querySelector

--- app.js
+++ app.js
@@ -18,26 +18,60 @@
 // getAddItem.style.color = 'green'
 
 //  BY CLASSNAME
-var items = document.getElementsByClassName('list-group-item');
-var allItems = document.getElementsByClassName('list-group')
+// var items = document.getElementsByClassName('list-group-item');
+// var allItems = document.getElementsByClassName('list-group')
 
-// console.log(items)
-// console.log(document.items[2]);
-items[2].textContent = 'Ferrari';
-// items[2].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green'
+// // console.log(items)
+// // console.log(document.items[2]);
+// items[2].textContent = 'Ferrari';
+// // items[2].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green'
 
-// give error
-// allItems.style.fontWeight = 'bold';
+// // give error
+// // allItems.style.fontWeight = 'bold';
 
-for(let i=0; i<items.length; i++){
-    items[i].style.fontWeight = 'bold'
-}
+// for(let i=0; i<items.length; i++){
+//     items[i].style.fontWeight = 'bold'
+// }
 
 // BY TAGNAME
 // let newItem = document.getElementsByClassName('new-item');
 // newItem[0].textContent = 'Maserati';
 
-let li = document.getElementsByTagName('li');
-li[4].textContent = 'Porsche';
+// let li = document.getElementsByTagName('li');
+// li[4].textContent = 'Porsche';
+
+// BY QUERYSELECTOR
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #000';
+
+// var addItems = document.querySelector('input[type="text"]');
+// addItems.value = 'hello';
+
+// var input = document.querySelector('input[type="submit"]');
+// input.value = 'SEND';
+
+// var item = document.querySelector('.list-group-item:nth-child(2)');
+// item.style.backgroundColor = 'green'
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
+// thirdItem.style.visibility = 'hidden';
+
+// BY QUERYSELECTORALL
+
+var secondItem = document.querySelectorAll('.list-group-item');
+secondItem[1].style.color = 'green';
+
+ // Another alternate way for odd & even elements.
+ // var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+
+for(var i=0; i<secondItem.length; i++){
+    if(i%2==0){
+        secondItem[i].style.backgroundColor = 'green';
+    }
+}
+
+
+
 

05f67ae03ddede68f3811190e326510f53d9ce49
akaAditya <mymail.adityapatel@gmail.com>
2023-05-21 02:04:14 +0530

    fifth commit modifying dom

--- app.js
+++ app.js
@@ -58,20 +58,100 @@
 // var thirdItem = document.querySelector('.list-group-item:nth-child(3)')
 // thirdItem.style.visibility = 'hidden';
 
-// BY QUERYSELECTORALL
+// // BY QUERYSELECTORALL
+// var secondItem = document.querySelectorAll('.list-group-item');
+// secondItem[1].style.color = 'green';
 
-var secondItem = document.querySelectorAll('.list-group-item');
-secondItem[1].style.color = 'green';
+//  // Another alternate way for odd & even elements.
+//  // var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
 
- // Another alternate way for odd & even elements.
- // var odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+// for(var i=0; i<secondItem.length; i++){
+//     if(i%2==0){
+//         secondItem[i].style.backgroundColor = 'green';
+//     }
+// }
 
-for(var i=0; i<secondItem.length; i++){
-    if(i%2==0){
-        secondItem[i].style.backgroundColor = 'green';
-    }
-}
+// TRAVERSING THE DOM
+
+var itemList = document.querySelector('#items');
+
+// parentNode
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.backgroundColor = 'yellow'
+// console.log(itemList.parentNode.parentNode);
+
+// parentElement
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.backgroundColor = 'yellow'
+// console.log(itemList.parentElement.parentElement);
+
+// childNodes
+// console.log(itemList.childNodes);
+
+// use "'children'" instead of 'chileNode'
+// console.log(itemList.children);
+// console.log(itemList.children[1]); // selects item no. 2 from itemlist
+// itemList.children[1].style.backgroundColor = 'cyan';
+
+// firstChild
+// console.log(itemList.firstChild)// return whitespace or element;
+
+// firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'hey there!';
+
+// lastChild
+// console.log(itemList.lastChild)// return whitespace or element;
+
+// lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.firstElementChild.textContent = 'hey there!';
+
+// // nextSibling
+// console.log(itemList.nextSibling);
+
+// // nextElementSibling
+// console.log(itemList.nextElementSibling); // use this instead of 'nextSibling'
+
+// // previousSibling
+// console.log(itemList.previousSibling);
+
+// // previousElementSibling
+// console.log(itemList.previousElementSibling); // use this instead of 'previousSibling'
 
 
 
+// createElement
 
+// create a div
+var newDiv = document.createElement('div');
+
+// add className
+newDiv.className = 'hello';
+
+// add id
+newDiv.id = 'hello1';
+
+// add attribute
+newDiv.setAttribute('title', 'Hello div');
+
+// create text node
+var newDivText = document.createTextNode('Hello World');
+
+// add text to div
+newDiv.appendChild(newDivText);
+
+// var container = document.querySelector('header .container ');
+// var h1 = document.querySelector('header h1');
+
+// // console.log(newDiv);
+// container.insertBefore(newDiv, h1)
+
+
+// add hello world before item 1
+
+var newdiv2 = document.querySelector('body #items');
+var li = document.querySelector('body li');
+
+console.log(newDiv);   
+newdiv2.insertBefore(newDiv, li);
